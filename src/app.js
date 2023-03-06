9dcacdf8d0aa08f0714f15a2048a9e48ed9bf3e0
TwiiiXeR <36134927+AshrafSamir@users.noreply.github.com>
2023-03-06 14:14:57 +0200

    Refactor: adding react

--- src/app.js
+++ src/app.js
@@ -8,14 +8,15 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname, "../templates/partials");
+// const publicDirectoryPath = path.join(__dirname, "../public");
+// const viewsPath = path.join(__dirname, "../templates/views");
+// const partialsPath = path.join(__dirname, "../templates/partials");
+const publicDirectoryPath = path.join(__dirname, "../build");
 
 // Setup handlebars engine and views location
-app.set("view engine", "hbs");
-app.set("views", viewsPath);
-hbs.registerPartials(partialsPath);
+// app.set("view engine", "hbs");
+// app.set("views", viewsPath);
+// hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
